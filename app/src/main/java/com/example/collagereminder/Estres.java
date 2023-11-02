3ac39634c672f16a93c21f4706b9242d348d1e36
Rmejia39 <sortomejia66@gmail.com>
2023-11-01 19:58:46 -0600

    Estres

---
+++ app/src/main/java/com/example/collagereminder/Estres.java
@@ -0,0 +1,14 @@
+package com.example.collagereminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Estres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estres);
+    }
+}
